5c16541b60e5069c1f5b7e11ffac9479e154c5ea
robgaston1 <robcoach0221@gmail.com>
2017-11-29 10:03:54 -0500

    add some components and their routes

---
+++ src/components/Landing.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-layout row>
+    <v-flex>
+      <h3>Landing page</h3>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+
+}
+</script>
---
+++ src/components/Signup.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-layout row>
+    <v-flex>
+      <h3>This is the sign up page</h3>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+
+}
+</script>
